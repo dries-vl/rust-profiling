538cf02ae9ad9a7010df3cfb95b6768d88cbb49d
Linux User <dries@DRIES-XPS-15>
2023-07-24 18:06:11 +0000

    stack size threads

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
+    std::env::set_var("RUSTFLAGS", "-C");
+    std::env::set_var("target-feature", "-crt-static");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::alloc::System;
 use jemallocator::Jemalloc;
 
 #[global_allocator]
@@ -7,6 +6,31 @@ static GLOBAL: Jemalloc = Jemalloc;
 fn main() {
     let start = &mut 0 as *mut i32;
     println!("Hello, world!");
+
+    test_stack();
+
     let end = &mut 0 as *mut i32;
     println!("Approximate stack usage: {} bytes", (end as usize) - (start as usize));
+}
+
+fn test_stack() {
+    let stack_size = 128 * 1024; // 128 kb
+    let builder = std::thread::Builder::new().stack_size(stack_size);
+
+    let handle = builder.spawn(|| {
+        test_func();
+    });
+
+    println!("Start of the program");
+    match handle.unwrap().join() {
+        Ok(_) => println!("Function executed successfully within stack size."),
+        Err(_) => println!("Stack overflowed!"),
+    }
+    println!("end of the program");
+}
+
+fn test_func() -> [i32; 16400] {
+    let mut array= [0; 16400];
+    array[0] = 1;
+    array
 }
