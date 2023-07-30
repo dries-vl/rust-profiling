d8ff400c858cc045831eda8fd4cf13bb1f0ea346
dries-vl <driesvanleuvenhaege@hotmail.com>
2023-07-30 17:59:40 +0200

    some messing with markdown

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,40 @@ mod stack_profiler;
 #[global_allocator]
 static GLOBAL: ProfilingAllocator = ProfilingAllocator;
 
+/// | @ this is a comment
+/// | # title
+/// | **bold text**
+/// |
+///
+/// | Header1 | Header2 |
+/// |---------|---------|
+/// | abc     | def     |
+///
+/// - [x] Complete task
+/// - [ ] Incomplete task
+/// ![Tip](images/tip.png)\
+///
+/// > ------------------------------------------------------------
+/// >  multiline text bla bla bla bla
+/// >  bla bla bla bla bla bla bla ... the
+/// >                       blank line below is important
+/// >   rtsrtrtrt
+/// > ------------------------------------------------------------
+///
+/// <div style="
+///         background: #F2F2F2;
+///         color: black;
+///         border: 3px solid #535353;
+///         margin: 0px auto;
+///         width: 456px;
+///         padding: 10px;
+///         border-radius: 10px;">
+///
+///     This is inside a **pretty** box.
+///
+/// </div>
+///
+///
 fn main() {
 
     stack_profiler::assert_stack_size(|| test_func(), 2765);
@@ -16,6 +50,16 @@ fn main() {
 
 }
 
+/// >
+/// > --------------------------------
+/// >
+/// >
+/// >
+/// >
+/// >
+/// >
+/// >
+/// > --------------------------------
 fn test_func() -> () {
     vec![1; 1000];
     let mut array = [0; 502];
