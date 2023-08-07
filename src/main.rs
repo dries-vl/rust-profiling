f875338a05baa73ac0062cfd02eb9919d788dd83
dries-vl <driesvanleuvenhaege@hotmail.com>
2023-08-07 21:56:59 +0200

    test graphics libraries for binary sizes

--- src/main.rs
+++ src/main.rs
@@ -41,20 +41,18 @@ static GLOBAL: ProfilingAllocator = ProfilingAllocator;
 ///
 ///
 fn main() {
-
     stack_profiler::assert_stack_size(|| test_func(), 2765);
     println!("Stack did not overflow!");
 
     let bytes = heap_profiler::measure_memory(|| test_func());
     println!("Allocated bytes: {}", bytes);
-
 }
 
-/// >
+/// ## title
 /// > --------------------------------
-/// >
-/// >
-/// >
+/// > comment
+/// comment
+/// **comment**
 /// >
 /// >
 /// >
